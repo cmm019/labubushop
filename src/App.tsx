9f19969667f8cee64b4b7e56ad7bab24b369ea70
cmm019 <cmurarolli@gmail.com>
2025-06-30 11:27:22 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-- import React, { useState, useRef, useEffect } from 'react';
-+ import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { 
   Heart, 
   Star, 
